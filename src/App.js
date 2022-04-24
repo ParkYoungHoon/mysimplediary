fc1779c8c9e102c5e82e559cd5bc123e1370058f
Tavi <secret9179@gmail.com>
2022-04-24 17:10:43 +0900

    데이터 추가하기 기능추가

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
 import './App.css';
+import React, { useRef, useState } from "react";
 import DiaryEditor from "./components/DiaryEditor";
 import DiaryList from "./components/DiaryList";
 
 function App() {
-  const diaryList = [
-    {
-      id: 1,
-      author: "작성자1",
-      content: "오늘의 일기1",
-      emotion: 1,
-      created_date: new Date().getTime(),
-    },
-    {
-      id: 2,
-      author: "작성자2",
-      content: "오늘의 일기2",
-      emotion: 2,
-      created_date: new Date().getTime(),
-    },
-    {
-      id: 3,
-      author: "작성자3",
-      content: "오늘의 일기3",
-      emotion: 3,
-      created_date: new Date().getTime(),
-    },
-  ];
+  const [data, setData] = useState([]);
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newDiary = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newDiary, ...data]);
+  };
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={diaryList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
