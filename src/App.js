20061832aaf8dbf86f36d24eb3fcf8b8629f68d9
Tavi <secret9179@gmail.com>
2022-04-24 11:00:25 +0900

    ADD DiaryEditor Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import DiaryEditor from "./components/DiaryEditor";
 
 function App() {
   return (
     <div className="App">
-      MySimpleDiary
+      <DiaryEditor />
     </div>
   );
 }
