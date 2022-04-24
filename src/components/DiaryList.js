0ecd88d5ddb7a722569c766f278d7057963ba378
Tavi <secret9179@gmail.com>
2022-04-24 16:21:47 +0900

    ADD DiaryItem Component

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,3 +1,6 @@
+import React from "react";
+import DiaryItem from "./DiaryItem";
+
 const DiaryList = ({ diaryList }) => {
   console.log(diaryList);
   return (
@@ -6,12 +9,7 @@ const DiaryList = ({ diaryList }) => {
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((it) => (
-          <div key={it.id}>
-            <div>작성자 : {it.author}</div>
-            <div>일기 본문 : {it.content}</div>
-            <div>감정 : {it.emotion}</div>
-            <div>작성일 : {it.created_date}</div>
-          </div>
+          <DiaryItem key={it.id} {...it} />
         ))}
       </div>
     </div>
